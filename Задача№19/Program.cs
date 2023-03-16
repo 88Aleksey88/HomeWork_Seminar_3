38dcec91b8a1536f521498882f7ee90a103b3ed6
Aleksey <Sad_libra1988@mail.ru>
2023-03-16 21:59:17 +0700

    Задача №19

---
+++ Задача№19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number > 9999 && number < 100000) || (number < -9999 && number > -100000))
+    {
+        if ((number / 10000 == number % 10) && (number / 1000 % 10 == number /10 % 10))
+            Console.WriteLine("Число полиндром");
+        else 
+            Console.WriteLine("Чиcло не полиндром");
+    }
+else 
+    Console.WriteLine("Число не пятизначное");
